7a44034514e372e7fb2bd395539b3135e632b497
Khiry Maynard <khirymaynard@macbookproislike.local>
2023-11-11 10:26:18 -0800

    complete array methods challenge

--- src/index.js
+++ src/index.js
@@ -35,8 +35,8 @@ function getCelcius2(){
     celciusArray = farenheit.map(value => (value - 32) * 5/9)
 }
 
-getCelcius2()
-console.log(celciusArray)
+// getCelcius2()
+// console.log(celciusArray)
 
 // Using 'some' write a function that checks an array for a 'falsey'
 // value.
@@ -44,16 +44,89 @@ console.log(celciusArray)
 
 // .some() test if at least one element in an array passes the test implemented by a provided function.
 
-const checkForFalsey = [11, NaN, [], {'Angels'}]
+const falsyArray = [11, NaN, [], 'Angels']
 
-console.log(checkForFalsey.some(item => item == true))
+const checkForFalsey = falsyArray.some(item => !item)
+console.log(checkForFalsey)
 
 
 
 // Using 'reduce' write a function that will return the total number
 // or characters in an arry of strings.
-// eg: getTotal(['Rabbit', 'Football', 'Cracking',]) => 22
+// eg: getTotal(['Rabbit', 'Football', 'Cracking']) => 22
 
-function getTotal() {
+const words = ['Rabbit', 'Football', 'Cracking']
+const getTotal = words.reduce((total, word) => total + word.length, 0)
+// 0 is because we are telling it to start from 0. we are giving it an initial value.
 
+console.log(getTotal)
+
+
+// Using `every` write a function that checks whether every number in
+// an array is a square number.
+// eg: checkSquares([19, 16, 81]) => true
+
+const numbers = [9, 16, 81]
+
+const checkSquares = numbers.every(numbers => Math.sqrt(numbers) % 1 === 0);
+
+console.log(checkSquares);
+
+// Using an array method, write a function that returns the string
+// elements of an array that have a given length or larger.
+// eg: getWords(['Florida', 'dog', 'phone'], 5) => ['Florida', 'phone']
+
+const words2 = ['Florida', 'dog', 'phone']
+
+const getWords = words2.filter(words2 => words2.length >= 5)
+
+console.log(getWords)
+
+// using an array method, write a function that converts an array of cm values
+// as strings, into an array of numbers.
+// eg: getValues(['23cm', '5.6cm', '1000cm']) => [23, 5.6, 1000]
+
+const cmValues = ['23cm', '5.6cm', '1000cm']
+
+function getValues() {
+    return cmValues.map(value => parseFloat(value))
 }
+
+console.log(getValues())
+
+// Option 2
+function getValues2(array) {
+    return array.map(value => parseFloat(value))
+}
+
+console.log(getValues2(['32cm', '6.5cm', '9000cm']))
+
+// Using `split` and `filter` write a function that counts the number of
+// vowels in a sentence.
+//eg: getVowelCount('In West Philadelphia, born and raised') => 12
+
+const saying = 'In West Philadelphia, born and raised'
+const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+const getVowelCount = saying.split('').filter(letter => vowels.includes(letter)).length
+
+console.log(getVowelCount)
+
+//Option 2 
+
+function getVowelCount2(sentance) {
+    return sentance.split('').filter(letter => vowels.includes(letter)).length
+} 
+
+console.log(getVowelCount2("I am a dog"))
+
+// Using `split`, `map` and `join` write a function that capitalizes the 
+// first letter of each word in a sentance.
+//eg: capitalise('the queens gambit') => 'The Queens Gambit"
+
+const title = 'the queens gambit'
+function capitalise(){
+    return title.split(' ').map(word => word.charAt(0).toUpperCase() + word.substr(1)).join(' ')
+}
+
+console.log(capitalise())
